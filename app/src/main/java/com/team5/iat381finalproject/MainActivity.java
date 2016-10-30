5e286d91952440dbc9e39a28e50698ea71f5af7d
wonglee5 <clw14@sfu.ca>
2016-10-29 21:10:24 -0700

    Added buttonSuggestions

--- app/src/main/java/com/team5/iat381finalproject/MainActivity.java
+++ app/src/main/java/com/team5/iat381finalproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.team5.iat381finalproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    protected void buttonSuggestions(View view) {
+        Uri webPage = Uri.parse("http://allrecipes.com/recipes/1947/everyday-cooking/quick-and-easy/");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webPage);
+        startActivity(webIntent);
+    }
+    
 }
