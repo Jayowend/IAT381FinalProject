05944131eabf9db1c7aa34af28c1847fb8c6e706
Jayowend <jayowend@gmail.com>
2016-11-02 18:54:42 -0700

    +Discard button finishes activity +Date Picker formatting when entering expiry date

--- app/src/main/java/com/team5/iat381finalproject/AddActivity.java
+++ app/src/main/java/com/team5/iat381finalproject/AddActivity.java
@@ -1,21 +1,60 @@
 package com.team5.iat381finalproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity{
 
     EditText nameEdit, expiredateEdit;
-//    database db;
+    private int year, month, day;
+
+    //    database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_item);
         nameEdit = (EditText) findViewById(R.id.Name_editText);
+
         expiredateEdit = (EditText) findViewById(R.id.date_editText);
+
+        // grab current date in calendar
+        final Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // expiry date formatting with DatePickerDialog
+        expiredateEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // new DatePickerDialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(view.getContext(), new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int pYear, int pMonth, int pDay) {
+                        // save date for next picker opening
+                        year = pYear;
+                        month = pMonth;
+                        day = pDay;
+
+                        // format date
+                        calendar.set(pYear, pMonth, pDay);
+                        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
+                        expiredateEdit.setText(sdf.format(calendar.getTime()));
+                    }
+                }, year, month, day);
+                datePickerDialog.setTitle("Select expiry date");
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    public void discardItem(View view) {
+        finish();
     }
 
     public void addItem (View view) {
@@ -36,4 +75,7 @@ public class AddActivity extends AppCompatActivity {
 //        latinName.setText("");
 //        location.setText("");
     }
+
+
+
 }
